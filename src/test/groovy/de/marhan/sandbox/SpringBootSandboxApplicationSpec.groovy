2ffb389fb8f4504e369c27c19486413866663eee
Markus Hanses <me@markushanses.de>
2015-10-11 22:19:09 +0200

    Spock Spec added.

---
+++ src/test/groovy/de/marhan/sandbox/SpringBootSandboxApplicationSpec.groovy
@@ -0,0 +1,59 @@
+package de.marhan.sandbox
+
+import org.springframework.boot.SpringApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.client.RestTemplate
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+
+class SpringBootSandboxApplicationSpec extends Specification {
+
+    @Shared
+    @AutoCleanup
+    ConfigurableApplicationContext context
+
+    void setupSpec() {
+        Future future = Executors
+                .newSingleThreadExecutor().submit(
+                new Callable() {
+                    @Override
+                    public ConfigurableApplicationContext call() throws Exception {
+                        return (ConfigurableApplicationContext) SpringApplication
+                                .run(SpringBootSandboxApplication.class)
+                    }
+                })
+        context = future.get(60, TimeUnit.SECONDS)
+    }
+
+    void "should return HttpStatus OK"() {
+        when:
+        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:12345/api/health", String.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+
+    }
+
+/*    void "should reverse request!"() {
+        when:
+        ResponseEntity<String> entity = new RestTemplate().getForEntity(url, String.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+        entity.body == reversedString
+
+        where:
+        url                                 || reversedString
+        'http://localhost:8080/reverse/uno' || 'onu'
+        'http://localhost:8080/reverse/ufc' || 'cfu'
+    }*/
+
+}
